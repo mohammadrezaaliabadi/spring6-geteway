d2b02096459f433aab60e7f5f23a98d5e9a29c70
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-27 23:20:11 +0330

    Initial commit

---
+++ src/main/java/com/example/spring6geteway/Spring6GetewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring6geteway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6GetewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6GetewayApplication.class, args);
+    }
+
+}
